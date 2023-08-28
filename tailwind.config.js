0b9973ebd81304aa6a45c63f1ad6967c13c27671
Igor Augusto <igoraugustobz@gmail.com>
2023-08-28 16:07:26 -0300

    style: personalização adicionada ao header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  content: [
+    './**/*.html',
+    '.**/*.html',
+  ],  theme: {
+    extend: {
+      fontFamily: {
+        'nunito': ['Nunito', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+      colors: {
+        'black-700': '#222222',
+        'blue-750': '#122ab2',
+        'blue-550': '#2C70FF',
+        
+      },
+      spacing: {
+        '10px': '10px',
+        '15px': '15px',
+      }
+    },
   },
   plugins: [],
 }
