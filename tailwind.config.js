197fa1f9203af192c6cc3e2312cd872672b5c241
Igor Augusto <igoraugustobz@gmail.com>
2023-08-30 15:36:33 -0300

    style: seção dos produtos personalizada

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         'black-700': '#222222',
         'blue-750': '#122ab2',
         'blue-550': '#2C70FF',
+        'blue-350': '#66D9EB',
+        'pink-650': '#F9265E',
+        'purple-450': '#AE81FF',
+        'gary-75': '#F7F7F7',
+        'gray-450': '#A4A4A4',
         'gray-550': '#7C7C7C',
         
       },
@@ -23,6 +28,7 @@ module.exports = {
       },
       flex: {
         '1.1': '1 1 160px',
+        '1.2': '1 1 175px',
         '2': '2 1 300px',
       },
     },
