5990f3d2197724d0ac62890fda7ca0deeb907727
Igor Augusto <igoraugustobz@gmail.com>
2023-08-30 06:15:52 -0300

    style: seção sobre personalizada

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,12 +12,19 @@ module.exports = {
         'black-700': '#222222',
         'blue-750': '#122ab2',
         'blue-550': '#2C70FF',
+        'gray-550': '#7C7C7C',
         
       },
       spacing: {
         '10px': '10px',
         '15px': '15px',
-      }
+        '20px': '20px',
+        '60px': '60px',
+      },
+      flex: {
+        '1.1': '1 1 160px',
+        '2': '2 1 300px',
+      },
     },
   },
   plugins: [],
